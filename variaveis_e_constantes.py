57f8114603fb94a744fdacb6c4d060fb435a758f
alessandradssmcruz <alenity@outlook.com>
2025-06-09 16:08:45 -0300

    comitando conversao e variaveis

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,22 @@
+nome = 'Alessandra'
+idade = 37
+print("Meu nome é", nome, "e tenho", idade, "anos.")
+
+nome, idade = 'Lilian', 42
+nome = 'Fernanda'
+print("Meu nome é", nome, "e tenho", idade, "anos.")
+
+limite_saque_diario = 1000.0
+print("O limite de saque diário é R$", limite_saque_diario)
+
+BRAZILIAN_STATES = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará',
+                   'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão',
+                   'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará',
+                   'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro',
+                   'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia',
+                   'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe',
+                   'Tocantins']
+print("Estados brasileiros:", ', '.join(BRAZILIAN_STATES))
+
+BRAZILIAN_STATES = 1000
+print("O valor de BRAZILIAN_STATES agora é", BRAZILIAN_STATES)
