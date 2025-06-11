432e21714e6698e9437fb97fb8e52cf139d14c3d
alessandradssmcruz <alenity@outlook.com>
2025-06-11 12:07:07 -0300

    modificação no código sobre indentação

--- indentacao_bloco.py
+++ indentacao_bloco.py
@@ -1,7 +1,13 @@
 def sacar(valor):
     saldo = 1000
     if valor <= saldo:
-        print("Valor sacado")
+        print("Saldo disponível")
+        print("Retire seu dinheiro na boca do caixa")
+    else:
+        print("Saldo insuficiente")
+        print("Tente um valor menor ou consulte o gerente")
+    
+    print("Obrigada por ser nosso cliente")
 
 
-sacar(500)  # Valor sacado
+sacar(1500)  # Valor sacado
